8ecc46944ca7c6b42ebcf0f9613429307e7fee20
kangsunghyuk98 <kangsh9820@naver.com>
2023-03-17 17:08:26 +0900

    시큐리티 설정

---
+++ src/main/java/com/nomalboard/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.nomalboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/css/**","/fonts/**","/img/**","/js/**");
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(login -> {
+
+                    login
+                            .loginPage("/login")
+                            .usernameParameter("username")
+                            .passwordParameter("password")
+                            .loginProcessingUrl("/login/success")
+                            .defaultSuccessUrl("/")
+                            .permitAll();
+
+                });
+
+        http.csrf().disable();
+        http
+                .authorizeRequests(authorize -> authorize
+                        .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                        .antMatchers("/member/**").authenticated()
+                        .anyRequest().permitAll()
+        );
+
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .deleteCookies("JSESSIONID");
+
+        return http.build();
+    }
+
+}
---
+++ src/main/java/com/nomalboard/controller/MemberController.java
@@ -0,0 +1,9 @@
+package com.nomalboard.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+
+}
