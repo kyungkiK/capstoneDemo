c10c93c388674d634ceb5ac98a8e6ca0840e7812
kangsunghyuk98 <kangsh9820@naver.com>
2023-03-17 15:31:58 +0900

    생성

---
+++ src/main/java/com/nomalboard/NomalboardApplication.java
@@ -0,0 +1,13 @@
+package com.nomalboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomalboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NomalboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nomalboard/NomalboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomalboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomalboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
